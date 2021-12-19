e3ee7ad2fe3a08f2eee3f15dddf471d64d8a87eb
FelipeReyesMi <89532512+FelipeReyesMi@users.noreply.github.com>
2021-12-19 02:41:49 -0600

    Add files via upload

---
+++ main.js
@@ -0,0 +1,18 @@
+document.querySelector(".menu-btn").addEventListener("click", () => {
+    document.querySelector(".nav-menu").classList.toggle("show");
+  });
+
+
+ScrollReveal().reveal('.showcase', { delay: 550 });
+ScrollReveal().reveal('.news-cards', { delay: 550 });
+ScrollReveal().reveal('.cards-banner-one', { delay: 550 });
+ScrollReveal().reveal('.news-cards', { delay: 550 });
+ScrollReveal().reveal('.cards-banner-two', { delay: 550 });
+ScrollReveal().reveal('.social', { delay: 550 });
+ScrollReveal().reveal('.footer-links', { delay: 550 });
+ScrollReveal().reveal('.footer', { delay: 550 });
+ScrollReveal().reveal('.integrantes', { delay: 550 });
+ScrollReveal().reveal('.letra', { delay: 550 });
+ScrollReveal().reveal('.imagentravel', { delay: 550 });
+ScrollReveal().reveal('.textopri', { delay: 550 });
+
